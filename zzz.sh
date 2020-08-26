32b46e703a5d395ffeaebd2abd872555c6cff15e
simurgh314 <62325871+simurgh314@users.noreply.github.com>
2020-08-26 12:01:26 +0500

    Delete zzz.sh

--- zzz.sh
+++
@@ -1,9 +0,0 @@
-
-rm -rf /etc/pacman.d/mirrorlist
-wget https://raw.githubusercontent.com/simurgh314/arch-testing/master/mirrorlist
-mv -f ~/mirrorlist /etc/pacman.d/mirrorlist
-pacstrap /mnt linux-firmware base base-devel linux linux-headers nano dhcpcd netctl dialog wpa_supplicant zsh grub efibootmgr
-
-genfstab -pU /mnt >> /mnt/etc/fstab
-
-arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/simurgh314/arch-testing/master/script2.sh)"
