a7118f0694994931fa753339573435422c932e79
simurgh314 <62325871+simurgh314@users.noreply.github.com>
2020-08-26 12:03:41 +0500

    Update format-mount.sh

--- format-mount.sh
+++ format-mount.sh
@@ -6,13 +6,14 @@ timedatectl set-ntp true
 
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4  /dev/sda2
-mkfs.ext4  /dev/sda3
+#mkfs.ext4  /dev/sda3
 
-echo '2.4.3 МОНТИРОВАНИЕ'
-mount /dev/sda2 /mnt
-mkdir /mnt/home
+
+mount /dev/mapper/container /mnt
+#mount /dev/sda2 /mnt
+#mkdir /mnt/home
 mkdir -p /mnt/boot/efi
 mount /dev/sda1 /mnt/boot/efi
-mount /dev/sda3 /mnt/home
+#mount /dev/sda3 /mnt/home
 
 lsblk
