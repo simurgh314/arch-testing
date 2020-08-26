a54a8256a68b30ee5ea5fa1491a72de890724d84
simurgh314 <62325871+simurgh314@users.noreply.github.com>
2020-08-26 17:08:41 +0500

    Update CRYPT.sh

--- CRYPT.sh
+++ CRYPT.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+echo 'GRUB_ENABLE_CRYPTODISK=y' >> /etc/default/grub
+
+#blkid -s UUID /dev/sda2 | grep -oP '(?<=UUID=").+?(?=")' 
 
 
-cryptsetup -yv luksFormat /dev/vda2
-cryptsetup open /dev/papper container
+#echo 'GRUB_CMDLINE_LINUX="cryptdevice=UUID=$CRYPTOID:cryptx"'
+
+
+mkinitcpio -p linux
+
+
+
+pacman -Syy
+pacman -S grub efibootmgr --noconfirm 
+grub-install /dev/vda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+#pacman -S netctl dialog wpa_supplicant --noconfirm 
+
+CRYPTOID=$(blkid -s UUID /dev/sda2 | grep -oP '(?<=UUID=").+?(?=")')
+sed -i "s/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="$CRYPTOID:container/g" >> /etc/default/grub
+
