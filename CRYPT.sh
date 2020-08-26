ff56756471712aa1909000abded2b2d2a803624e
simurgh314 <62325871+simurgh314@users.noreply.github.com>
2020-08-26 12:13:18 +0500

    Update CRYPT.sh

--- CRYPT.sh
+++ CRYPT.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-timedatectl set-ntp true
-
-(
- echo g;
-
- echo n;
- echo ;
- echo ;
- echo +300M;
- echo Y;
- echo t;
- echo 1;
-
-  
- echo n;
- echo;
- echo;
- echo;
- echo Y;
-  
- echo w;
-) | fdisk -t gpt /dev/vda
-
-
-mkfs.fat -F32 /dev/vda1
-mkfs.ext4  /dev/vda2
-
 
 cryptsetup -yv luksFormat /dev/vda2
 cryptsetup open /dev/papper container
