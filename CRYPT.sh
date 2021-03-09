b3acd437702c230a5df44cb97f0e13aaf86c30d7
Anton <62325871+simurgh314@users.noreply.github.com>
2021-03-09 22:21:52 +0500

    Update CRYPT.sh

--- CRYPT.sh
+++ CRYPT.sh
@@ -2,20 +2,12 @@
 echo 'GRUB_ENABLE_CRYPTODISK=y' >> /etc/default/grub
 
 #blkid -s UUID /dev/sda2 | grep -oP '(?<=UUID=").+?(?=")' 
-
-
 #echo 'GRUB_CMDLINE_LINUX="cryptdevice=UUID=$CRYPTOID:cryptx"'
-
-
 #mkinitcpio -p linux
-
-
-
 #pacman -Syy
 #pacman -S grub efibootmgr --noconfirm 
 #grub-install /dev/vda
 #grub-mkconfig -o /boot/grub/grub.cfg
-
 #pacman -S netctl dialog wpa_supplicant --noconfirm 
 
 CRYPTOID=$(blkid -s UUID /dev/sda2 | grep -oP '(?<=UUID=").+?(?=")')
